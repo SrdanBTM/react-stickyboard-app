363e34bbe957d843f872348befeca9d36bc483ef
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-10-10 19:32:21 +0200

    feat: add components to Board

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -4,8 +4,14 @@ import styles from './board.module.css'
 
 export default function Board() {
   return (
-    <section className={styles.board}>
-      board
+    <section className={styles.container}>
+      <div className={styles.heading}>
+        <p>Razno</p>
+      </div>
+
+      <div className={styles.board}>
+        
+      </div>
     </section>
   )
 }
