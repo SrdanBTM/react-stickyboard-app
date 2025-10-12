93c2e86391f4e4893d0378b3edac5678763cb4bc
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-10-12 16:36:28 +0200

    feat: add edit button to board list item

--- src/components/boards-list/BoardsList.jsx
+++ src/components/boards-list/BoardsList.jsx
@@ -1,22 +1,44 @@
 
 
 import styles from './boardsList.module.css'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { MainContext } from '../../context-provider/ContextProvider.jsx'
 
 export default function BoardsList() {
 
   const BASE_URL = import.meta.env.BASE_URL
 
+  const [isMouseOver, setIsMouseOver] = useState(false)
+
   const { boards } = useContext(MainContext)
+
+  function handleMouseOver() {
+    setIsMouseOver(true)
+  }
+
+  function handleMouseLeave() {
+    setIsMouseOver(false)
+  }
+
+  const editButton =
+    <div className={styles.editButton}>
+      <img src={`${BASE_URL}images/icon-edit2.png`} alt="edit" />
+    </div>
+
 
   return (
     <div>
       {boards.map((board, index) => {
         return (
-          <div key={index} className={styles.boardName}>
+          <div
+            key={index}
+            className={styles.boardName}
+            onMouseOver={handleMouseOver}
+            onMouseLeave={handleMouseLeave}
+          >
             <p>{board.boardName}</p>
-            <img src={`${BASE_URL}images/icon-edit1.png`} alt="edit" />
+
+            {isMouseOver && editButton}
           </div>
 
         )
