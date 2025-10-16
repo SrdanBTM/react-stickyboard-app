12b9247036bab14db9db00b28c02ab82b18de802
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-10-16 11:46:32 +0200

    feat: add functionality to NewSticker.jsx

--- src/components/new-sticker/NewSticker.jsx
+++ src/components/new-sticker/NewSticker.jsx
@@ -1,15 +1,45 @@
 
 import styles from './newSticker.module.css'
+import { useContext, useEffect } from 'react'
+import { MainContext } from '../../context-provider/ContextProvider.jsx'
 
 export default function NewSticker() {
+
+  const { setBoards, currentBoard, stickerTemplate } = useContext(MainContext)
+
+
+
+  function handleMouseDown() {
+    const sticker = {
+      ...stickerTemplate,
+      boardName: currentBoard.boardName,
+      color: 'var(--sticker-color1)'
+    }
+
+    setBoards(prev => {
+      return (
+        prev.map(board => {
+          return (
+            board.boardName === currentBoard.boardName
+              ? { ...board, stickers:[...board.stickers, sticker] }
+              : board
+          )
+        })
+      )
+    })
+  }
+
   return (
     <div className={styles.container}>
-        <div className={styles.heading}>
-          <p>Stickers</p>
-        </div>
+      <div className={styles.heading}>
+        <p>Stickers</p>
+      </div>
 
-      <div className={styles.newSticker}>
-        
+      <div
+        className={styles.newSticker}
+        onMouseDown={handleMouseDown}
+      >
+
       </div>
     </div>
   )
