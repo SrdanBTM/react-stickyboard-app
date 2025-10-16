d96baa3d6cfca4b7808b62b384fde388751c2cd4
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-10-16 11:49:23 +0200

    feat: add functionality - render stickers in board

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -6,15 +6,27 @@ import styles from './board.module.css'
 export default function Board() {
 
   const { currentBoard } = useContext(MainContext)
+  console.log(currentBoard);
+  
 
   return (
     <section className={styles.container}>
       <div className={styles.heading}>
-        <p>{currentBoard && currentBoard.boardName}</p>
+        <p>{currentBoard.boardName}</p>
       </div>
 
       <div className={styles.board}>
-
+        {currentBoard.stickers.map((sticker, index) => {
+          return (
+            <div
+              key={index}
+              className={styles.sticker}
+              style={{backgroundColor: sticker.color}}
+            >
+              {index}
+            </div>
+          )
+        })}
       </div>
     </section>
   )
