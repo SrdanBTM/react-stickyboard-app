0a5e137d0ce28cb9825e3c1bb9b4f5972308ea70
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-10-20 17:54:26 +0200

    feat: move templates to new file

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,12 @@
 
 import Header from './components/header/Header.jsx'
 import Main from './components/main/Main.jsx'
+import { boardTemplate, stickerTemplate } from './templates/Templates.jsx'
 import { MainProvider } from './context-provider/ContextProvider.jsx'
 import { useState, useEffect } from 'react'
 
 
 export default function App() {
-
-  const boardTemplate = {
-    boardName: 'My Board',
-    isInput: false,
-    isFocused: false,
-    stickers: []
-  }
-
-  const stickerTemplate = {
-    heading: '',
-    content: '',
-    color: '',
-    boardName: '',
-    positionX: '',
-    positionY: ''
-  }
-
 
   const [boards, setBoards] = useState([boardTemplate])
   const [currentBoard, setCurrentBoard] = useState(boards[0])
---
+++ src/templates/Templates.jsx
@@ -0,0 +1,17 @@
+
+
+export const boardTemplate = {
+  boardName: 'My Board',
+  isInput: false,
+  isFocused: false,
+  stickers: []
+}
+
+export const stickerTemplate = {
+  heading: '',
+  content: '',
+  color: '',
+  boardName: '',
+  positionX: '',
+  positionY: ''
+}
