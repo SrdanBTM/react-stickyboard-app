3c69d0d2b28a8d52e6bfd6526d8e7822352b589d
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-10-22 09:52:12 +0200

    feat: adjust icon image and prop

--- src/components/main/sidebar/new-board/board-list/board-list-span/board-name-dots/BoardNameDots.jsx
+++ src/components/main/sidebar/new-board/board-list/board-list-span/board-name-dots/BoardNameDots.jsx
@@ -5,11 +5,10 @@ import { useContext } from 'react'
 import { MainContext } from '../../../../../../../context-provider/ContextProvider.jsx'
 
 
-export default function boardNameDots({ boardId }) {
+export default function boardNameDots({ board }) {
 
   const { setBoards, theme } = useContext(MainContext)
   const BASE_URL = import.meta.env.BASE_URL
-
 
 
 
@@ -30,12 +29,13 @@ export default function boardNameDots({ boardId }) {
     <div
       className={styles.container}
       onClick={handleClick}
-      data-id={boardId}
+      data-id={board.boardId}
     >
+      {}
       <img src={
         theme === 'darkTheme'
-          ? `${BASE_URL}images/icon-dots2.png`
-          : `${BASE_URL}images/icon-dots1.png`}
+          ? `${BASE_URL}images/icon-delete2.png`
+          : `${BASE_URL}images/icon-delete1.png`}
         alt="edit" />
     </div>
   )
