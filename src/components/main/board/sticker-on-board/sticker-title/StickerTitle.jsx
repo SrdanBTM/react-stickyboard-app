08ac8ef589c13afc5ebe1507a4d225e73ca06231
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-04 19:57:31 +0100

    refactor: remove span from StickerTitle

--- src/components/main/board/sticker-on-board/sticker-title/StickerTitle.jsx
+++ src/components/main/board/sticker-on-board/sticker-title/StickerTitle.jsx
@@ -7,46 +7,20 @@ import { useState } from 'react'
 export default function StickerHeader() {
 
   const [inputValue, setInputValue] = useState('')
-  const [isInput, setIsInput] = useState(true)
 
 
   function handleChange(e) {
     setInputValue(e.target.value)
   }
 
-  function handleKeyDown(e) {
-    if (e.key === 'Enter') setIsInput(false)
-  }
-
-  function handleDoubleClick() {
-    setIsInput(true)
-  }
-
-  
   return (
-    <div
-      className={styles.container}
-      onDoubleClick={handleDoubleClick}
-    >
-
-      {isInput
-
-        ? <div className={styles.input}>
-          <input
-            type="text"
-            placeholder="Title..."
-            value={inputValue}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-          />
-        </div>
-
-        : <div className={styles.span}>
-          <span>
-            {inputValue}
-          </span>
-        </div>
-      }
+    <div className={styles.container}>
+      <input
+        type="text"
+        placeholder="Title..."
+        value={inputValue}
+        onChange={handleChange}
+      />
     </div>
   )
 } 
