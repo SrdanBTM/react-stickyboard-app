9cc9732ff7ae7020b1233acd83a2669f289623de
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-05 14:28:40 +0100

    feat: add logic delete sticker

--- src/components/main/board/sticker-on-board/colors-palette/delete-sticker/DeleteSticker.jsx
+++ src/components/main/board/sticker-on-board/colors-palette/delete-sticker/DeleteSticker.jsx
@@ -1,14 +1,39 @@
 
 
 import styles from './deleteSticker.module.css'
+import { useContext } from 'react'
+import { MainContext } from '../../../../../../context-provider/ContextProvider.jsx'
 
 
-export default function DeleteSticker() {
+export default function DeleteSticker({ stickerId }) {
 
   const BASE_URL = import.meta.env.BASE_URL
+  const { setBoards, currentBoardId } = useContext(MainContext)
+
+
+  function handleClick(e) {
+    const currentStickerId = e.currentTarget.getAttribute('data-id')
+
+    setBoards(prev => prev.map(board => {
+      return (
+        board.boardId === currentBoardId
+        ? {
+          ...board,
+          stickers: board.stickers.filter(sticker => sticker.stickerId !== currentStickerId)
+        }
+        : board
+      )
+    }))
+
+
+  }
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      data-id={stickerId}
+      onClick={handleClick}
+    >
       <img src={`${BASE_URL}images/icon-delete3.png`} alt="delete sticker" />
     </div>
   )
