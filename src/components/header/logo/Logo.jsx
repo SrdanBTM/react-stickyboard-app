51a9a1e10573a9634f30c70f0b389941bfe92c1f
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-07 19:43:21 +0100

    style: adjust logo color for themes

--- src/components/header/logo/Logo.jsx
+++ src/components/header/logo/Logo.jsx
@@ -1,13 +1,18 @@
 
 import styles from './logo.module.css'
+import { useContext } from 'react'
+import { MainContext } from '../../../context-provider/ContextProvider.jsx'
 
 export default function Logo() {
 
   const BASE_URL = import.meta.env.BASE_URL
+  const { theme } = useContext(MainContext)
 
   return (
     <div className={styles.container}>
-      <img src={`${BASE_URL}images/icon-logo2.png`} alt="logo image" />
+      <img
+        src={`${BASE_URL}${theme === 'darkTheme' ? 'images/icon-logo3.png' : 'images/icon-logo4.png'}`}
+        alt="logo image" />
       <span>StickyBoard</span>
     </div>
   )
