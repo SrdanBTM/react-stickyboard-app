2dfc523dbba056d0275d37b27b62ca7664e2fb6e
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-08 14:05:37 +0100

    feat: update filter component

--- src/components/header/filter/Filter.jsx
+++ src/components/header/filter/Filter.jsx
@@ -1,11 +1,28 @@
 
 
 import styles from './filter.module.css'
+import { useContext, useState } from 'react'
+import { MainContext } from '../../../context-provider/ContextProvider.jsx'
 
 export default function Filter() {
+
+  const { boards } = useContext(MainContext)
+  const [inputValue, setInputValue] = useState('')
+
+
+  function handleChange(e) {
+    setInputValue(e.target.value)
+  }
+
+
   return (
     <div className={styles.container}>
-      <input type="text" />
+      <input
+        type="text"
+        placeholder='Find a sticker by title...'
+        onChange={handleChange}
+        value={inputValue}
+      />
     </div>
   )
 }
