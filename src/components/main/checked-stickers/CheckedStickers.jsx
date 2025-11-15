107d0d518bd6cd066917fd4ea21166a8ef2bb56f
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-15 13:58:33 +0100

    feat: add StickerOnCheckedStickers

--- src/components/main/checked-stickers/CheckedStickers.jsx
+++ src/components/main/checked-stickers/CheckedStickers.jsx
@@ -1,12 +1,13 @@
 
 
 import styles from './checkedStickers.module.css'
+import StickerOnCheckedStickers from './sticker-on-checked-stickers/StickerOnCheckedStickers.jsx'
 
 
 export default function CheckedStickers() {
   return (
     <div className={styles.container}>
-      
+      <StickerOnCheckedStickers />
     </div>
   )
 }
---
+++ src/components/main/checked-stickers/sticker-on-checked-stickers/StickerOnCheckedStickers.jsx
@@ -0,0 +1,12 @@
+
+
+import styles from './stickerOnCheckedStickers.module.css'
+
+
+export default function StickerOnCheckedStickers() {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  )
+}
