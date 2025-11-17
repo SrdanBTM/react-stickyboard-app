ac73c9d2df9712ecc612e95410ed640412161990
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-17 18:42:30 +0100

    update: add conditionally CheckedStickerPanel render

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,13 +3,18 @@ import styles from './main.module.css'
 import Sidebar from './sidebar/Sidebar.jsx'
 import Board from './board/Board.jsx'
 import CheckedStickers from './checked-stickers-panel/CheckedStickersPanel.jsx'
+import { useContext } from 'react'
+import { MainContext } from '../../context-provider/ContextProvider.jsx'
 
 export default function Main() {
+
+  const { isSearchResultBoard } = useContext(MainContext)
+
   return (
     <main className={styles.container}>
       <Sidebar />
       <Board />
-      <CheckedStickers />
+      {!isSearchResultBoard && <CheckedStickers />}
     </main>
   )
 }
