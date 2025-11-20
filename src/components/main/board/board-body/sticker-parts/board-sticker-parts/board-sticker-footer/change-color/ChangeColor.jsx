100d43b9d32d0b0a7d9e60f3d21a95d2fb6a6f78
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-20 13:52:27 +0100

    feat: import MainContext, add conditionaly zIndex

--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-footer/change-color/ChangeColor.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-footer/change-color/ChangeColor.jsx
@@ -4,16 +4,20 @@
 import styles from './changeColor.module.css'
 import ColorsPaletteIcon from './colors-palette-icon/ColorsPaletteIcon.jsx'
 import ColorsPalette from './colors-palette/ColorsPalette.jsx'
-import { useState } from 'react'
+import { useState, useContext, useEffect } from 'react'
+import { MainContext } from '../../../../../../../../context-provider/ContextProvider.jsx'
 
 
 export default function ChangeColor({ mappedSticker }) {
 
+  const { } = useContext(MainContext)
   const [isMouseOver, setIsMouseOver] = useState(false)
+
 
   function handleMouseOver() {
     setIsMouseOver(true)
   }
+
 
   function handleMouseOut() {
     setIsMouseOver(false)
@@ -25,7 +29,10 @@ export default function ChangeColor({ mappedSticker }) {
       className={styles.container}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
-      style={{ transform: isMouseOver ? 'translateX(0px)' : 'translateX(210px)' }}
+      style={{
+        transform: isMouseOver ? 'translateX(0px)' : 'translateX(210px)',
+        zIndex: isMouseOver ? '1' : '0'
+      }}
     >
       <ColorsPaletteIcon />
       <ColorsPalette mappedSticker={mappedSticker} />
