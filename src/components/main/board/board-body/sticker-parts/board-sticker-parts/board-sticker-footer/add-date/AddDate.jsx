661d155f8d158541447265e200cf6c107ed6632d
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-23 11:10:36 +0100

    update: remove logic from AddDate

--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-footer/add-date/AddDate.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-footer/add-date/AddDate.jsx
@@ -2,34 +2,19 @@
 import { useState, useContext, useEffect } from 'react'
 import { MainContext } from '../../../../../../../../context-provider/ContextProvider.jsx'
 import styles from './addDate.module.css'
-import AddDateCalendar from './add-date-calendar/AddDateCalendar.jsx'
 import AddDateIcon from './add-date-icon/AddDateIcon.jsx'
 
 
 export default function AddDate({ mappedSticker }) {
 
   const { } = useContext(MainContext)
-  const [isMouseOver, setIsMouseOver] = useState(false)
-
-
-  function handleMouseOver() {
-    setIsMouseOver(true)
-  }
-
-
-  function handleMouseOut() {
-    setIsMouseOver(false)
-  }
 
 
   return (
     <div
       className={styles.container}
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
     >
       <AddDateIcon />
-      <AddDateCalendar />
     </div>
   )
 }
