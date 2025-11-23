fe9bb1903177766f8ce71089fee688279526f2cb
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-23 12:02:23 +0100

    chore: remove old AddDate and rename AddDateElement to AddDate

--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-footer/add-date/AddDate.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-footer/add-date/AddDate.jsx
@@ -1,24 +1,22 @@
 
-import { useState, useContext, useEffect } from 'react'
-import { MainContext } from '../../../../../../../../context-provider/ContextProvider.jsx'
-import styles from './addDate.module.css'
-import AddDateIcon from './add-date-icon/AddDateIcon.jsx'
+
+import styles from './addDateElement.module.css'
 
 
-export default function AddDate({ mappedSticker }) {
-
-  const { } = useContext(MainContext)
-
-
+export default function AddDateElement() {
   return (
-    <div
-      className={styles.container}
-    >
-      <AddDateIcon />
+    <div className={styles.container}>
+      <div className={styles.inputs}>
+        <input type="text" placeholder='Write date' />
+        <input type="text" placeholder='Write time' />
+      </div>
+
+      <span>22.11.2025  |  Saturday  |  10:31</span>
+
+      <div className={styles.buttons}>
+        <button className={styles.saveButton}>SAVE</button>
+        <button className={styles.closeButton}>CLOSE</button>
+      </div>
     </div>
   )
 }
-
-
-
-
