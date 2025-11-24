6473c097737dc7cdf2cbbd1f63c8da87db995fa2
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-24 15:17:27 +0100

    feat: add onClick logic

--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-footer/add-date-icon/AddDateIcon.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-footer/add-date-icon/AddDateIcon.jsx
@@ -1,13 +1,25 @@
 
 import styles from './addDateIcon.module.css'
+import { useContext } from 'react'
+import { MainContext } from '../../../../../../../../context-provider/ContextProvider.jsx'
 
 
-export default function AddDateIcon() {
+export default function AddDateIcon({ mappedSticker }) {
 
   const BASE_URL = import.meta.env.BASE_URL
+  const { setIsAddDateOpen } = useContext(MainContext)
+
+
+  function handleClick() {
+    setIsAddDateOpen(mappedSticker.stickerId)
+  }
+
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onClick={handleClick}
+    >
       <div className={styles.image}>
         <img src={`${BASE_URL}images/icon-date2.png`} alt="add date" />
       </div>
--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-modals/add-date/AddDate.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-modals/add-date/AddDate.jsx
@@ -1,21 +1,57 @@
 
 
-import styles from './addDateElement.module.css'
+import styles from './addDate.module.css'
+import { useContext } from 'react'
+import { MainContext } from '../../../../../../../../context-provider/ContextProvider.jsx'
 
 
-export default function AddDateElement() {
+export default function AddDate({ mappedSticker }) {
+
+  const { isAddDateOpen, setIsAddDateOpen } = useContext(MainContext)
+
+
+  function handleClickSave() {
+    setIsAddDateOpen(false)
+  }
+
+
+  function handleClickClose() {
+    setIsAddDateOpen(false)
+  }
+
+
   return (
-    <div className={styles.container}>
-      <div className={styles.inputs}>
-        <input type="text" placeholder='Write date' />
-        <input type="text" placeholder='Write time' />
-      </div>
+    <div
+      className={styles.container}
+      style={{
+        transform: isAddDateOpen === mappedSticker.stickerId ? 'scale(1)' : 'scale(0)',
+        transition: 'transform 0.3s ease',
+        backgroundColor: mappedSticker.color
+      }}
+    >
+      <div className={styles.content}>
+        <div className={styles.inputs}>
+          <input type="text" placeholder='Write date' />
+          <input type="text" placeholder='Write time' />
+        </div>
 
-      <span>22.11.2025  |  Saturday  |  10:31</span>
+        <span>22.11.2025  |  Saturday  |  10:31</span>
 
-      <div className={styles.buttons}>
-        <button className={styles.saveButton}>SAVE</button>
-        <button className={styles.closeButton}>CLOSE</button>
+        <div className={styles.buttons}>
+          <button
+            className={styles.saveButton}
+            onClick={handleClickSave}
+          >
+            SAVE
+          </button>
+
+          <button
+            className={styles.closeButton}
+            onClick={handleClickClose}
+          >
+            CLOSE
+          </button>
+        </div>
       </div>
     </div>
   )
