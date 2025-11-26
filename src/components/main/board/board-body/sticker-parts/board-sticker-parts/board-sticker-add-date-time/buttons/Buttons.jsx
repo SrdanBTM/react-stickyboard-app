ddd5f8c243b1271176a16bb9bd726eda78663ec6
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-26 09:48:52 +0100

    feat: update sticker on Save

--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-add-date-time/buttons/Buttons.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-add-date-time/buttons/Buttons.jsx
@@ -5,13 +5,21 @@ import { useContext } from 'react'
 import { MainContext } from '../../../../../../../../context-provider/ContextProvider.jsx'
 
 
-export default function Buttons() {
+export default function Buttons({ mappedSticker }) {
 
-  const { setIsAddDateOpen, setValidatedValueDate, setValidatedValueTime, setInputValueDate, setInputValueTime } = useContext(MainContext)
+  const { boards, updateSticker, setBoards, currentBoardId, setIsAddDateOpen, dayInWeek, validatedValueDate, validatedValueTime, setValidatedValueDate, setValidatedValueTime, setInputValueDate, setInputValueTime } = useContext(MainContext)
 
 
   function handleClickSave() {
     setIsAddDateOpen(null)
+
+    const currentStickerId = mappedSticker.stickerId
+    const propertyToUpdate1 = { key: 'scheduledDate', value: validatedValueDate }
+    const propertyToUpdate2 = { key: 'scheduledTime', value: validatedValueTime }
+    const propertyToUpdate3 = { key: 'scheduledDayInWeek', value: dayInWeek }
+    updateSticker(setBoards, currentBoardId, currentStickerId, propertyToUpdate1)
+    updateSticker(setBoards, currentBoardId, currentStickerId, propertyToUpdate2)
+    updateSticker(setBoards, currentBoardId, currentStickerId, propertyToUpdate3)
   }
 
 
