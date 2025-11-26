130a7a3b0d50563829011eda5705e67120bbc333
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-26 09:47:12 +0100

    feat: implement dayInWeek state

--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-add-date-time/message/Message.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-add-date-time/message/Message.jsx
@@ -7,7 +7,7 @@ import { MainContext } from '../../../../../../../../context-provider/ContextPro
 
 export default function Message() {
 
-  const { validatedValueDate, validatedValueTime } = useContext(MainContext)
+  const { validatedValueDate, validatedValueTime, dayInWeek } = useContext(MainContext)
 
 
   const invalidFormatMessage = 'Invalid date or time format.'
@@ -15,13 +15,10 @@ export default function Message() {
 
   if (validatedValueDate && validatedValueDate !== 'init' 
     && validatedValueTime && validatedValueTime !== 'init' ) {
-    const [year, month, day] = validatedValueDate.trim().split('.')
-    const dayInWeek = new Date(year, month - 1, day).getDay()
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
     confirmMessage = validatedValueDate !== false
       && validatedValueTime !== false
-      && `${validatedValueDate} | ${days[dayInWeek - 1]} | ${validatedValueTime}`
+      && `${validatedValueDate} | ${dayInWeek} | ${validatedValueTime}`
   }
 
 
