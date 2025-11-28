5f3170a0270bce0c5f70db153d6dfbb80058a11e
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-28 13:55:33 +0100

    feat: add props to children components

--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-add-date-time/inputs/Inputs.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-add-date-time/inputs/Inputs.jsx
@@ -3,14 +3,17 @@
 import styles from './inputs.module.css'
 import DateInput from './date-input/DateInput.jsx'
 import TimeInput from './time-input/TimeInput.jsx'
+import { useContext, useEffect } from 'react'
+import { MainContext } from '../../../../../../../../context-provider/ContextProvider.jsx'
 
 
-export default function Inputs() {
+export default function Inputs({ mappedSticker }) {
+
 
   return (
     <div className={styles.container}>
-      <DateInput />
-      <TimeInput />
+      <DateInput mappedSticker={mappedSticker} />
+      <TimeInput mappedSticker={mappedSticker} />
     </div>
   )
 }
