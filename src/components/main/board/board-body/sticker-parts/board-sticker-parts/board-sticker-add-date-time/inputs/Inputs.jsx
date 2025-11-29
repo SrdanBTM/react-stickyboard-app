6e117de3035fc62409a9908e65554b622524b39c
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-11-29 19:23:50 +0100

    refactor: remove MainContext

--- src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-add-date-time/inputs/Inputs.jsx
+++ src/components/main/board/board-body/sticker-parts/board-sticker-parts/board-sticker-add-date-time/inputs/Inputs.jsx
@@ -3,12 +3,9 @@
 import styles from './inputs.module.css'
 import DateInput from './date-input/DateInput.jsx'
 import TimeInput from './time-input/TimeInput.jsx'
-import { useContext, useEffect } from 'react'
-import { MainContext } from '../../../../../../../../context-provider/ContextProvider.jsx'
 
 
 export default function Inputs({ mappedSticker }) {
-
 
   return (
     <div className={styles.container}>
