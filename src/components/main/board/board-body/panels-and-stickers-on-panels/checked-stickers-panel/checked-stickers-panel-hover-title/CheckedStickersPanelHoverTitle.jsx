69e70b0698c623cc46d4e2c6233052eee21c5145
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-02 16:09:49 +0100

    feat: add CheckedStickerPanelHoverTitle and module.css

---
+++ src/components/main/board/board-body/panels-and-stickers-on-panels/checked-stickers-panel/checked-stickers-panel-hover-title/CheckedStickersPanelHoverTitle.jsx
@@ -0,0 +1,12 @@
+
+
+import styles from './checkedStickersPanelHoverTitle.module.css'
+
+
+export default function CheckedStickersPanelHoverTitle() {
+  return (
+    <div className={styles.container}>
+      <span>Show Checked Stickers</span>
+    </div>
+  )
+}
