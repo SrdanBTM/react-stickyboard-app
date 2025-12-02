9adc071800efb945999a5e8003a7ccd230a3514b
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-02 14:47:30 +0100

    feat: remove UserIcon and UserMenu from component

--- src/components/header/user-and-settings/UserAndSettings.jsx
+++ src/components/header/user-and-settings/UserAndSettings.jsx
@@ -4,8 +4,6 @@ import { useContext, useState, useRef, useEffect } from 'react'
 import styles from './userAndSettings.module.css'
 import SettingsMenu from './settings-menu/SettingsMenu.jsx'
 import SettingsIcon from './settings-icon/SettingsIcon.jsx'
-import UserMenu from './user-menu/UserMenu.jsx'
-import UserIcon from './user-icon/UserIcon.jsx'
 import { MainContext } from '../../../context-provider/ContextProvider.jsx'
 
 
@@ -31,11 +29,6 @@ export default function UserAndSettings() {
 
   return (
     <div className={styles.container}>
-
-      <div className={styles.user}>
-        <UserIcon setClickedElement={setClickedElement} />
-        <UserMenu clickedElement={clickedElement} />
-      </div>
 
       <div className={styles.settings}>
         <SettingsIcon setClickedElement={setClickedElement} />
