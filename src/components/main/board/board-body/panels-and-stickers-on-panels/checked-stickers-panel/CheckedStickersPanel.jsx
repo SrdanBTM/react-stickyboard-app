732831f5ba437dec3c68504cd9a04a1c7e3977a7
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-02 16:15:24 +0100

    refactor: update | add: implement CheckedStickersPanelHoverTitle

--- src/components/main/board/board-body/panels-and-stickers-on-panels/checked-stickers-panel/CheckedStickersPanel.jsx
+++ src/components/main/board/board-body/panels-and-stickers-on-panels/checked-stickers-panel/CheckedStickersPanel.jsx
@@ -3,7 +3,8 @@
 
 import styles from './checkedStickersPanel.module.css'
 import StickerOnCheckedStickersPanel from './sticker-on-checked-stickers-panel/StickerOnCheckedStickersPanel.jsx'
-import { useContext, useState } from 'react'
+import CheckedStickersPanelHoverTitle from './checked-stickers-panel-hover-title/CheckedStickersPanelHoverTitle.jsx'
+import { useContext, useState, useEffect } from 'react'
 import { MainContext } from '../../../../../../context-provider/ContextProvider.jsx'
 
 
@@ -11,34 +12,64 @@ export default function CheckedStickersPanel() {
 
   const { boards, currentBoardId, isCheckedStickersPanelShow } = useContext(MainContext)
   const [randomUUID, setRandomUUID] = useState(crypto.randomUUID())
+  const [isHoverTitleShow, setIsHoverTitleShow] = useState(true)
+  const [isCheckedStickersPanelHover, setIsCheckedStickersPanelHover] = useState(false)
+
 
   let currentBoard = null
   if (boards.length > 0) {
     currentBoard = boards.find(board => board.boardId === currentBoardId)
   }
 
+  const filteredAndSortedCheckedStickers =
+    currentBoard && currentBoard.stickers
+      .filter(sticker => sticker.checked)
+      .sort((sticker1, sticker2) => sticker1.checkedOrder - sticker2.checkedOrder)
+
+
+  useEffect(() => {
+    setIsCheckedStickersPanelHover(false)
+    setTimeout(() => {
+      setIsHoverTitleShow(true)
+    }, 300)
+  }, [filteredAndSortedCheckedStickers.length])
+
+
+  function handleMouseOver() {
+    setIsCheckedStickersPanelHover(true)
+    setIsHoverTitleShow(false)
+  }
+
+
+  function handleMouseLeave() {
+    setIsCheckedStickersPanelHover(false)
+    setTimeout(() => {
+      setIsHoverTitleShow(true)
+    }, 300)
+  }
+
 
   return (
     <div
       className={styles.container}
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
       style={{
-        right: isCheckedStickersPanelShow ? '0' : '-200px'
+        right: isCheckedStickersPanelShow || isCheckedStickersPanelHover ? '0' : '-200px'
       }}
     >
-      {currentBoard
-        && currentBoard.stickers
-          .filter(sticker => sticker.checked)
-          .sort((sticker1, sticker2) => sticker1.checkedOrder - sticker2.checkedOrder)
-          .map((sticker, index) => {
-            return (
-              <StickerOnCheckedStickersPanel
-                key={sticker.stickerId + randomUUID}
-                topPosition={20 + index * 40}
-                mappedSticker={sticker}
-                setRandomUUID={setRandomUUID}
-              />
-            )
-          })}
+      {filteredAndSortedCheckedStickers.map((sticker, index) => {
+        return (
+          <StickerOnCheckedStickersPanel
+            key={sticker.stickerId + randomUUID}
+            topPosition={20 + index * 40}
+            mappedSticker={sticker}
+            setRandomUUID={setRandomUUID}
+          />
+        )
+      })}
+
+      {!isCheckedStickersPanelShow && isHoverTitleShow && <CheckedStickersPanelHoverTitle />}
 
     </div>
   )
