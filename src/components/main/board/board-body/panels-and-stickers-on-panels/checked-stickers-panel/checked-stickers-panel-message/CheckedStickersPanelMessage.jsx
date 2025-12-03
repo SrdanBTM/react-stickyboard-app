c22b98349eb7516616babe2108b58047275c080f
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-03 11:29:25 +0100

    feat: add CheckedStickersPanelMessage.jsx and css

---
+++ src/components/main/board/board-body/panels-and-stickers-on-panels/checked-stickers-panel/checked-stickers-panel-message/CheckedStickersPanelMessage.jsx
@@ -0,0 +1,12 @@
+
+
+import styles from './checkedStickersPanelMessage.module.css'
+
+
+export default function CheckedStickersPanelMessage() {
+  return (
+    <div className={styles.container}>
+      <span>No checked stickers</span>
+    </div>
+  )
+}
