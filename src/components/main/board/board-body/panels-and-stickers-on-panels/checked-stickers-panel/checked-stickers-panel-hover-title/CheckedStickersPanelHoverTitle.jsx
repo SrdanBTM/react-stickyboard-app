6e51323d139e8d50e9fb194a6c53ea9525819aae
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-03 11:33:37 +0100

    chore: adjust title text

--- src/components/main/board/board-body/panels-and-stickers-on-panels/checked-stickers-panel/checked-stickers-panel-hover-title/CheckedStickersPanelHoverTitle.jsx
+++ src/components/main/board/board-body/panels-and-stickers-on-panels/checked-stickers-panel/checked-stickers-panel-hover-title/CheckedStickersPanelHoverTitle.jsx
@@ -6,7 +6,7 @@ import styles from './checkedStickersPanelHoverTitle.module.css'
 export default function CheckedStickersPanelHoverTitle() {
   return (
     <div className={styles.container}>
-      <span>Show Checked Stickers</span>
+      <span>Show checked stickers</span>
     </div>
   )
 }
