5978a85c6725faba3ac52d6d536ff4279d192ec9
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-03 11:30:25 +0100

    feat: add CheckedStickersPanelHeader.jsx and css

---
+++ src/components/main/board/board-body/panels-and-stickers-on-panels/checked-stickers-panel/checked-stickers-panel-header/CheckedStickersPanelHeader.jsx
@@ -0,0 +1,14 @@
+
+
+
+import styles from './checkedStickersPanelHeader.module.css'
+
+
+export default function CheckedStickersPanelHeader() {
+
+  return (
+    <div className={styles.container  }>
+      <span>{'Checked stickers'}</span>
+    </div>
+  )
+} 
