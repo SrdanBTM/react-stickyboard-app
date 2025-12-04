88f5196b44c5c92edcf35a6bc61ac01297ba075f
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-04 10:37:46 +0100

    feat: add ShawAll.jsx and css

---
+++ src/components/header/filter/show-all/ShowAll.jsx
@@ -0,0 +1,14 @@
+
+
+import styles from './showAll.module.css'
+
+
+export default function ShowAll() {
+  return (
+    <div className={styles.container}>
+      <button>
+        All
+      </button>
+    </div>
+  )
+}
