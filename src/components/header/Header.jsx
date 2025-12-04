4a2d0b67a1d4188506f37e1aca3fb00a30b6ee8a
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-04 10:36:28 +0100

    feat: implement Filter.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@
 import styles from './header.module.css'
 import Logo from './logo/Logo.jsx'
 import SearchBar from './search-bar/SearchBar.jsx'
+import Filter from './filter/Filter.jsx'
 import UserAndSettings from './user-and-settings/UserAndSettings.jsx'
 
 export default function Header() {
@@ -10,6 +11,7 @@ export default function Header() {
     <header className={styles.header}>
       <Logo />
       <SearchBar />
+      <Filter />
       <UserAndSettings />
     </header>
   )
