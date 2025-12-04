c01d5deffebe2d4ae4f3411c3a9959e133abbe9d
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-04 11:16:25 +0100

    feat: add handleClick to set setIsFilterDatedMenuShow

--- src/components/header/filter/dated-menu-icon/DatedMenuIcon.jsx
+++ src/components/header/filter/dated-menu-icon/DatedMenuIcon.jsx
@@ -1,14 +1,26 @@
 
 
 import styles from './datedMenuIcon.module.css'
+import { useContext } from 'react'
+import { MainContext } from '../../../../context-provider/ContextProvider.jsx'
 
 
 export default function DatedMenuIcon() {
 
+  const { setIsFilterDatedMenuShow } = useContext(MainContext)
   const BASE_URL = import.meta.env.BASE_URL
 
+
+  function handleClick() {
+    setIsFilterDatedMenuShow(prev => !prev)
+  }
+
+
   return (
-    <div className={styles.container}>
+    <div 
+    className={styles.container}
+    onClick={handleClick}
+    >
       <img src={`${BASE_URL}images/icon-down-arrow1.png`} alt='down arrow' />
     </div>
   )
