f2873c677ebf620a598a7c87d3e07af42527a83c
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-05 08:08:09 +0100

    feat: update FilteredStickerPanel.jsx and css

--- src/components/main/board/board-body/panels-and-stickers-on-panels/filtered-stickers-panel/FilteredStickersPanel.jsx
+++ src/components/main/board/board-body/panels-and-stickers-on-panels/filtered-stickers-panel/FilteredStickersPanel.jsx
@@ -1,12 +1,33 @@
 
 
 import styles from './filteredStickersPanel.module.css'
+import { useContext, useRef } from 'react'
+import { MainContext } from '../../../../../../context-provider/ContextProvider.jsx'
+import StickerOnFilteredStickersPanel from './sticker-on-filtered-stickers-panel/StickerOnFilteredStickersPanel.jsx'
 
 
 export default function FilteredStickersPanel() {
-  return (
-    <div className={styles.container}>
 
+  const { boards } = useContext(MainContext)
+
+
+  const stickersWithValidDateTime = boards.flatMap(board => board.stickers.filter(sticker => sticker.isDateTimeValid))
+
+
+  return (
+    <div
+      className={styles.container}
+    >
+      {stickersWithValidDateTime.map(sticker => {
+        return (
+          <StickerOnFilteredStickersPanel
+            key={sticker.stickerId}
+            mappedSticker={sticker}
+          />
+        )
+      })}
     </div>
   )
 }
+
+
