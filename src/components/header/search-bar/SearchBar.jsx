1ebfabb74215a7e157ff0b92aedf76b122999056
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-05 08:06:33 +0100

    feat: implement setIsFilterResultBoard state

--- src/components/header/search-bar/SearchBar.jsx
+++ src/components/header/search-bar/SearchBar.jsx
@@ -6,18 +6,19 @@ import { MainContext } from '../../../context-provider/ContextProvider.jsx'
 
 export default function SearchBar() {
 
-  const { updateAllStickers, setBoards, searchValue, setSearchValue, setIsSearchResultBoard, setCurrentBoardId, currentBoardId } = useContext(MainContext)
+  const { updateAllStickers, setBoards, searchValue, setSearchValue, setIsSearchResultBoard, setIsFilterResultBoard, setCurrentBoardId, currentBoardId } = useContext(MainContext)
 
   function handleChange(e) {
-    const propertyToUpdate1 = {key: 'isAddDateTimeModalOpen', value: false}
-    const propertyToUpdate2 = {key: 'isChangeColorModalOpen', value: false}
-    const propertyToUpdate3 = {key: 'isDeleteModalOpen', value: false}
+    const propertyToUpdate1 = { key: 'isAddDateTimeModalOpen', value: false }
+    const propertyToUpdate2 = { key: 'isChangeColorModalOpen', value: false }
+    const propertyToUpdate3 = { key: 'isDeleteModalOpen', value: false }
     updateAllStickers(setBoards, currentBoardId, propertyToUpdate1)
     updateAllStickers(setBoards, currentBoardId, propertyToUpdate2)
     updateAllStickers(setBoards, currentBoardId, propertyToUpdate3)
 
     setSearchValue(e.target.value)
     setIsSearchResultBoard(true)
+    setIsFilterResultBoard(false)
     setCurrentBoardId(null)
   }
 
