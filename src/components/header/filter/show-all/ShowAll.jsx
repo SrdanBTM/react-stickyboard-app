391a2f11dd6186d5cf55d42f5682f8e567707dc9
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-05 08:05:14 +0100

    feat: implement AllButton in ShowAll

--- src/components/header/filter/show-all/ShowAll.jsx
+++ src/components/header/filter/show-all/ShowAll.jsx
@@ -1,14 +1,14 @@
 
 
 import styles from './showAll.module.css'
+import AllButton from './all-button/AllButton.jsx'
 
 
 export default function ShowAll() {
+
   return (
     <div className={styles.container}>
-      <button>
-        All
-      </button>
+      <AllButton />
     </div>
   )
 }
