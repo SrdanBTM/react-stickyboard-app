ea22c4c96e051e33db24bb1e4ccd891fdf708850
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-06 20:01:52 +0100

    feat: add MainModal.jsx and css

--- src/components/modals/main-modal/MainModal.jsx
+++ src/components/modals/main-modal/MainModal.jsx
@@ -3,17 +3,25 @@
 import styles from './mainModal.module.css'
 
 
-export default function MainModal() {
+export default function MainModal({ messages, buttons }) {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
 
         <div className={styles.messages}>
-          {messages.map((message, index) => <p key={index}>{message}</p>)}
+          {messages.map((msg, index) => <p key={index}>{msg}</p>)}
         </div>
 
         <div className={styles.buttons}>
-          {buttons.map((button, index) => <button>{button.title}</button>)}
+          {buttons.map((btn, index) => (
+            <button
+              key={index}
+              className={styles.button}
+              onClick={btn.onClick}
+            >
+              {btn.title}
+            </button>)
+          )}
         </div>
 
       </div>
