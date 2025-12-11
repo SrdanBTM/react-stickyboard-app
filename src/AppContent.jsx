b8c652e41092b6b9d531af287fe5e06c1880825e
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-11 09:31:36 +0100

    feat: implement openedAppModal state from AppModalsContext / import and implement modal components

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -3,19 +3,32 @@
 import Header from './components/header/Header.jsx'
 import Main from './components/main/Main.jsx'
 import { useContext } from 'react'
-import { ClickOutsideElementContext } from './contexts/ClickOutsideElementContext.jsx'
 import { MainContext } from './context-provider/ContextProvider.jsx'
+import { ClickOutsideElementContext } from './contexts/ClickOutsideElementContext.jsx'
+import { AppModalsContext } from './contexts/AppModalsContext.jsx'
+import DeleteBoardModal from './components/app-modals/app-modals/DeleteBoardModal.jsx'
+import DeleteAllCheckedStickersModal from './components/app-modals/app-modals/DeleteAllCheckedStickersModal.jsx'
+import MessageNoCheckedStickerToDeleteModal from './components/app-modals/app-modals/MessageNoCheckedStickersToDeleteModal.jsx'
 
 
 export default function AppContent() {
 
-  const { setClickedElementOnApp } = useContext(ClickOutsideElementContext)
   const { theme } = useContext(MainContext)
+  const { setClickedElementOnApp } = useContext(ClickOutsideElementContext)
+  const { openedAppModal } = useContext(AppModalsContext)
 
 
   function handleClick(e) {
     setClickedElementOnApp(e.target)
   }
+
+
+  const appModals = {
+    DeleteBoardModal,
+    DeleteAllCheckedStickersModal,
+    MessageNoCheckedStickerToDeleteModal
+  }
+  const AppModal = appModals[openedAppModal]
 
 
   return (
@@ -27,6 +40,7 @@ export default function AppContent() {
 
       <Header />
       <Main />
+      {openedAppModal && <AppModal />}
 
     </div>
   )
