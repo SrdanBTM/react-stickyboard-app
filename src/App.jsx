8653d06a4ebd45f043d9c00a5fbaae991bdd28ae
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-12 12:10:26 +0100

    feat: add MainProvider.jsx / move states from App.jsx to MainProvider.jsx / implement MainProvider.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,21 @@
 
 
-import { useState, useRef } from 'react'
-
-
-// providers 
-import { MainProvider } from './context-provider/ContextProvider.jsx'
+import AppContent from './AppContent.jsx'
+import MainProvider from './providers/MainProvider.jsx'
 import ClickOutsideElementProvider from './providers/ClickOutsideElementProvider.jsx'
 import AppModalsProvider from './providers/AppModalsProvider.jsx'
 import ThemeProvider from './providers/ThemeProvider.jsx'
 
 
-// components
-import AppContent from './AppContent.jsx'
-
-
-// data
-import dummyData from './dummy-data/dummyData.json'
-// const [currentBoardId, setCurrentBoardId] = useState(id)
-
-
-
-
 export default function App() {
-  
-  // import { boardTemplate } from './templates/Templates.jsx'
-  // const id = crypto.randomUUID()
-  // const [boards, setBoards] = useState([{ ...boardTemplate, boardId: id }])
-  const [boards, setBoards] = useState(dummyData)
-
-  const currentBoardIdInit = boards.length > 0 ? boards[0].boardId : null
-  const [currentBoardId, setCurrentBoardId] = useState(currentBoardIdInit)
-
-
-  const [searchValue, setSearchValue] = useState('')
-  const [checkedStickerId, setCheckedStickerId] = useState(null)
-  const [isCheckedStickersPanelShow, setIsCheckedStickersPanelShow] = useState(false)
-  const [currentBoardPanel, setCurrentBoardPanel] = useState('board')
-
-
-  const boardRef = useRef()
-
 
   return (
     <ThemeProvider>
       <AppModalsProvider>
         <ClickOutsideElementProvider>
-          <MainProvider
-            value={{
-              boardRef,
-              boards, setBoards,
-              currentBoardId, setCurrentBoardId,
-              currentBoardPanel, setCurrentBoardPanel,
-              searchValue, setSearchValue,
-              checkedStickerId, setCheckedStickerId,
-              isCheckedStickersPanelShow, setIsCheckedStickersPanelShow,
+          <MainProvider>
 
-            }}>
-
-            <AppContent />
+              <AppContent />
 
           </MainProvider>
         </ClickOutsideElementProvider>
---
+++ src/providers/MainProvider.jsx
@@ -0,0 +1,47 @@
+
+
+import { useState, useRef } from 'react'
+import { MainContext } from '../contexts/MainContext.jsx'
+import dummyData from '../dummy-data/dummyData.json'
+import { boardTemplate } from '../templates/Templates.jsx'
+
+
+
+export default function MainProvider({ children }) {
+
+  // const id = crypto.randomUUID()
+  // const [boards, setBoards] = useState([{ ...boardTemplate, boardId: id }])
+  const [boards, setBoards] = useState(dummyData)
+  
+
+  // const [currentBoardId, setCurrentBoardId] = useState(id)
+  const currentBoardIdInit = boards.length > 0 ? boards[0].boardId : null
+  const [currentBoardId, setCurrentBoardId] = useState(currentBoardIdInit)
+
+
+  const [searchValue, setSearchValue] = useState('')
+  const [checkedStickerId, setCheckedStickerId] = useState(null)
+  const [isCheckedStickersPanelShow, setIsCheckedStickersPanelShow] = useState(false)
+  const [currentBoardPanel, setCurrentBoardPanel] = useState('board')
+
+
+  const boardRef = useRef()
+
+
+  return (
+    <MainContext.Provider
+      value={{
+        boardRef,
+        boards, setBoards,
+        currentBoardId, setCurrentBoardId,
+        currentBoardPanel, setCurrentBoardPanel,
+        searchValue, setSearchValue,
+        checkedStickerId, setCheckedStickerId,
+        isCheckedStickersPanelShow, setIsCheckedStickersPanelShow,
+
+      }}
+    >
+      {children}
+    </MainContext.Provider>
+  )
+}
