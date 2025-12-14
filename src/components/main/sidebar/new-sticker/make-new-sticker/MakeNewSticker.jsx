a45dcc1f89c2e73d68701aea8d2920ca6501af73
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-14 11:28:13 +0100

    feat: update MakeNewSticker.jsx to open modal if currentBoardPanel !== board

--- src/components/main/sidebar/new-sticker/make-new-sticker/MakeNewSticker.jsx
+++ src/components/main/sidebar/new-sticker/make-new-sticker/MakeNewSticker.jsx
@@ -3,15 +3,17 @@
 import styles from './makeNewSticker.module.css'
 import { useContext, useRef, useState, useEffect } from 'react'
 import { MainContext } from '../../../../../contexts/MainContext.jsx'
+import { AppModalsContext } from '../../../../../contexts/AppModalsContext.jsx'
 import { motion } from 'framer-motion'
-import { addSticker } from '../../../../../helper-functions/HelperFunctionsHandleSticker.jsx' 
+import { addSticker } from '../../../../../helper-functions/HelperFunctionsHandleSticker.jsx'
 import { stickerTemplate } from '../../../../../templates/Templates.jsx'
 
 
 export default function MakeNewSticker() {
 
   const [key, setKey] = useState(0)
-  const { boards, boardRef, setBoards, currentBoardId } = useContext(MainContext)
+  const { setOpenedAppModal } = useContext(AppModalsContext)
+  const { boards, boardRef, setBoards, currentBoardId, currentBoardPanel } = useContext(MainContext)
   const stickerRef = useRef()
   const [isStickerDragged, setIsStickerDragged] = useState(false)
 
@@ -30,11 +32,14 @@ export default function MakeNewSticker() {
     const stickerPositionInBoardBottom = boardPosition.bottom - stickerPosition.bottom
 
     if (stickerPositionInBoardLeft >= 0 && stickerPositionInBoardTop >= 0
-      && stickerPositionInBoardRight >= 0 && stickerPositionInBoardBottom >= 0) {
-        const boardName = currentBoard.boardName
-        const positionXValue = stickerPositionInBoardLeft
-        const positionYValue = stickerPositionInBoardTop
-        addSticker(setBoards, currentBoardId, stickerTemplate, boardName, positionXValue, positionYValue)
+      && stickerPositionInBoardRight >= 0 && stickerPositionInBoardBottom >= 0
+      && currentBoardPanel === 'board') {
+      const boardName = currentBoard.boardName
+      const positionXValue = stickerPositionInBoardLeft
+      const positionYValue = stickerPositionInBoardTop
+      addSticker(setBoards, currentBoardId, stickerTemplate, boardName, positionXValue, positionYValue)
+    } else if (currentBoardPanel !== 'board') {
+      setOpenedAppModal('MessageNewStickerEnableOnlyOnBoardPanel')
     }
 
     setKey(prev => prev + 1)
@@ -53,7 +58,7 @@ export default function MakeNewSticker() {
       style={{
         width: isStickerDragged ? '250px' : '',
         height: isStickerDragged ? '250px' : '',
-        cursor: boardRef.current ? 'grab' : 'auto' 
+        cursor: boardRef.current ? 'grab' : 'auto'
       }}
       drag={boardRef.current ? true : false}
       dragMomentum={false}
