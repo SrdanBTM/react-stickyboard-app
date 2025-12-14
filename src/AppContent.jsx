3744071ef90af2556ebed791b18eb25cc9a91aa5
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-14 11:25:47 +0100

    feat: implement MessageNewStickerEnableOnlyOnBoardPanel.jsx in AppContent.jsx

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -9,6 +9,7 @@ import { ThemeContext } from './contexts/ThemeContext.jsx'
 import DeleteBoardModal from './components/app-modals/app-modals/DeleteBoardModal.jsx'
 import DeleteAllCheckedStickersModal from './components/app-modals/app-modals/DeleteAllCheckedStickersModal.jsx'
 import MessageNoCheckedStickerToDeleteModal from './components/app-modals/app-modals/MessageNoCheckedStickersToDeleteModal.jsx'
+import MessageNewStickerEnableOnlyOnBoardPanel from './components/app-modals/app-modals/MessageNewStickerEnableOnlyOnBoardPanel.jsx'
 
 
 export default function AppContent() {
@@ -26,7 +27,8 @@ export default function AppContent() {
   const appModals = {
     DeleteBoardModal,
     DeleteAllCheckedStickersModal,
-    MessageNoCheckedStickerToDeleteModal
+    MessageNoCheckedStickerToDeleteModal,
+    MessageNewStickerEnableOnlyOnBoardPanel
   }
   const AppModal = appModals[openedAppModal]
 
