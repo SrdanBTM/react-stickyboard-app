033faf7eb21b1562f502b0d3c5a938d491bc0d30
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-15 09:00:52 +0100

    feat: add MainPanelFilterDatedNextDays.jsx

---
+++ src/components/main/board/board-body/main-panels/MainPanelFilterDatedNextDays.jsx
@@ -0,0 +1,20 @@
+
+import { useContext } from 'react'
+import { MainContext } from '../../../../../contexts/MainContext.jsx'
+import MainPanelsBase from '../main-panels-base/MainPanelsBase.jsx'
+
+
+export default function MainPanelFilter() {
+
+  const { boards } = useContext(MainContext)
+
+  const filteredStickers = boards.flatMap(board => board.stickers.filter(sticker => sticker.isDateTimeValid))
+
+
+  return (
+    <MainPanelsBase
+      stickers={filteredStickers}
+    />
+  )
+}
+
