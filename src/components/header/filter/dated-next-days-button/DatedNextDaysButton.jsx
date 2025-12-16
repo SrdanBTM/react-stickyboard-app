daff733bc6e501cd6b8aed29d59714b7ca19b1e8
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-16 14:58:34 +0100

    feat: add datedNextDaysNummber state for button title / add handleClick and useEffect

--- src/components/header/filter/dated-next-days-button/DatedNextDaysButton.jsx
+++ src/components/header/filter/dated-next-days-button/DatedNextDaysButton.jsx
@@ -2,18 +2,46 @@
 
 import styles from './datedNextDaysButton.module.css'
 import { MainContext } from '../../../../contexts/MainContext.jsx'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 
 export default function DatedNextDaysButton() {
 
-  const { datedNextDaysValue } = useContext(MainContext)
+  const [datedNextDaysNumber, setDatedNextDaysNumber] = useState(3)
+  const {
+    datedNextDaysValue,
+    setDatedNextDaysValue,
+    setCurrentBoardPanel,
+    setCurrentBoardId,
+    setSearchValue,
+    selectedFilterButton, setSelectedFilterButton } = useContext(MainContext)
+
+
+  useEffect(() => {
+    if (datedNextDaysValue === undefined) return
+    setDatedNextDaysNumber(prev => datedNextDaysValue === 'all' ? prev : datedNextDaysValue)
+  }, [datedNextDaysValue])
+
+
+  function handleClick() {
+    setCurrentBoardId(null)
+    setSearchValue('')
+    setCurrentBoardPanel('filter')
+    setSelectedFilterButton('datedNextDays')
+    setDatedNextDaysValue(datedNextDaysNumber)
+  }
 
 
   return (
     <div className={styles.container}>
-      <button>
-        {`Dated - Next ${datedNextDaysValue} days`}
+      <button
+        onClick={handleClick}
+        style={{
+          border: selectedFilterButton === 'datedNextDays' ? '1px solid rgba(255,255,255,0.5)' : '',
+          borderRadius: selectedFilterButton === 'datedNextDays' ? '5px' : ''
+        }}
+      >
+        {`Dated - Next ${datedNextDaysNumber} days`}
       </button>
     </div>
   )
