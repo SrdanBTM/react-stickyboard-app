22b72a9527826ef4a381892d2869dac5276e5fcb
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-16 11:33:30 +0100

    feat: add onChange and value atributes

--- src/components/main/board/board-body/main-panels-sticker-parts/add-date-time-modal/input/Input.jsx
+++ src/components/main/board/board-body/main-panels-sticker-parts/add-date-time-modal/input/Input.jsx
@@ -3,13 +3,15 @@
 import styles from './input.module.css'
 
 
-export default function Input({ placeholder }) {
+export default function Input({ placeholder, handleChange, inputName, value }) {
   return (
     <div className={styles.container}>
       <input
         type="text"
         maxLength={2}
         placeholder={placeholder}
+        onChange={(e) => handleChange(inputName, e)}
+        value={value}
       />
     </div>
 
