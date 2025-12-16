e00b6c619f1ea1d07bcf7502a8379c40b9650ad8
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-16 17:01:44 +0100

    feat: update MainPanelFilter.jsx to update filteredStickers array with next days values

--- src/components/main/board/board-body/main-panels/MainPanelFilter.jsx
+++ src/components/main/board/board-body/main-panels/MainPanelFilter.jsx
@@ -1,15 +1,39 @@
 
 
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { MainContext } from '../../../../../contexts/MainContext.jsx'
 import MainPanelsBase from '../main-panels-base/MainPanelsBase.jsx'
 
 
 export default function MainPanelFilter() {
 
-  const { boards } = useContext(MainContext)
+  const { boards, datedNextDaysValue } = useContext(MainContext)
 
-  const filteredStickers = boards.flatMap(board => board.stickers.filter(sticker => sticker.isDateTimeValid))
+  const today = new Date()
+  const todayDay = today.getDate()
+  const todayMonth = today.getMonth() + 1
+  const todayYear = today.getFullYear()
+
+
+  let filteredStickers = []
+
+
+  if (datedNextDaysValue === 'all') {
+    filteredStickers = boards.flatMap(board => board.stickers.filter(sticker => sticker.isDateTimeValid))
+
+  } else if (typeof datedNextDaysValue === 'number') {
+    const maxDay = todayDay + datedNextDaysValue
+
+    const daysInCurrentMonth = new Date(todayYear, todayMonth, 0).getDate()
+
+    filteredStickers = boards.flatMap(board =>
+      board.stickers.filter(sticker => 
+        sticker.isDateTimeValid && sticker.dateTimeValidValue.day >= todayDay && sticker.dateTimeValidValue.day <= maxDay)
+    )
+  }
+
+
+
 
 
   return (
