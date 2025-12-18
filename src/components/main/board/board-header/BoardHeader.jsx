d46851913714efc12bbfc8dc31c665678b362308
Srdan Cakalj <srdjancakalj14@gmail.com>
2025-12-18 09:25:46 +0100

    feat: add if statement for title

--- src/components/main/board/board-header/BoardHeader.jsx
+++ src/components/main/board/board-header/BoardHeader.jsx
@@ -7,20 +7,30 @@ import { MainContext } from '../../../../contexts/MainContext.jsx'
 
 export default function BoardHeader() {
 
-  const { currentBoardPanel, currentBoardId, boards } = useContext(MainContext)
+  const { currentBoardPanel, currentBoardId, boards, datedNextDaysValue, searchValue } = useContext(MainContext)
 
   const currentBoard =
-  boards.length > 0
-    ? boards.find(board => board.boardId === currentBoardId)
-    : null
+    boards.length > 0
+      ? boards.find(board => board.boardId === currentBoardId)
+      : null
+
+
+  let title = null
+
+  if (currentBoardPanel === 'board') {
+    title = currentBoard.boardName
+  } else if (currentBoardPanel === 'search') {
+    title = `Search results for '${searchValue}'`
+  } else if (currentBoardPanel === 'filter' && typeof datedNextDaysValue === 'number') {
+    title = `Dated stickers · Next ${datedNextDaysValue} days`
+  } else if (currentBoardPanel === 'filter' && datedNextDaysValue === 'all') {
+    title = `Dated stickers · All`
+  }
 
 
   return (
     <div className={styles.container}>
-      {currentBoardPanel === 'search'
-        ? <span>Search results</span>
-        : currentBoard && <span>{currentBoard.boardName}</span>
-      }
+      <span>{title}</span>
     </div>
   )
 }
