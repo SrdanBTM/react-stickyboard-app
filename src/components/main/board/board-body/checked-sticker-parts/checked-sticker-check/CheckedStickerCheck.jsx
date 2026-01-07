cef481736d51ee14576b479bb28bef8d95d48ea5
Srdan Cakalj <srdjancakalj14@gmail.com>
2026-01-07 09:24:46 +0100

    style: update styles

--- src/components/main/board/board-body/checked-sticker-parts/checked-sticker-check/CheckedStickerCheck.jsx
+++ src/components/main/board/board-body/checked-sticker-parts/checked-sticker-check/CheckedStickerCheck.jsx
@@ -6,8 +6,6 @@ import styles from './checkedStickerCheck.module.css'
 
 export default function CheckedStickerCheck() {
 
-  const BASE_URL = import.meta.env.BASE_URL
-
   return (
     <div className={styles.container}>
         <IconCheckboxChecked size={16} />
