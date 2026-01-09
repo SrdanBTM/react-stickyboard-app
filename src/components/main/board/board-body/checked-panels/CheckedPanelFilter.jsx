75632792f5f64c791799fc4b8e28b08326f67117
Srdan Cakalj <srdjancakalj14@gmail.com>
2026-01-09 17:25:35 +0100

    feat: sort checked sticker on filter panel by date

--- src/components/main/board/board-body/checked-panels/CheckedPanelFilter.jsx
+++ src/components/main/board/board-body/checked-panels/CheckedPanelFilter.jsx
@@ -10,17 +10,27 @@ export default function CheckedPanelFilter() {
   const { boards } = useContext(MainContext)
 
 
-  const stickersToShowOnCheckedPanel = boards.flatMap(board =>
+  const filteredStickers = boards.flatMap(board =>
     board.stickers.filter(sticker =>
       sticker.checked === true &&
       sticker.isDateTimeValid === true
     )
   )
+
+  const sortedStickers = filteredStickers.sort((a, b) => {
+    const da = a.dateTimeValidValue
+    const db = b.dateTimeValidValue
+  
+    const dateA = new Date(da.year, da.month - 1, da.day)
+    const dateB = new Date(db.year, db.month - 1, db.day)
+  
+    return dateA - dateB 
+  })
   
 
   return (
     <CheckedPanelBase
-      stickersToShowOnCheckedPanel={stickersToShowOnCheckedPanel}
+      stickersToShowOnCheckedPanel={sortedStickers}
     />
   )
 }
