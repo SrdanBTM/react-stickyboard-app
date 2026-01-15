8be4e1181b73ba08fcb7373c6ba552216d6c48a4
Srdan Cakalj <srdjancakalj14@gmail.com>
2026-01-15 14:13:38 +0100

    feat: add localStorage to getItem and setItem

--- src/providers/MainProvider.jsx
+++ src/providers/MainProvider.jsx
@@ -8,22 +8,42 @@ import { boardTemplate } from '../templates/Templates.jsx'
 
 export default function MainProvider({ children }) {
 
-  const [boards, setBoards] = useState(() => [{ ...boardTemplate, boardId: crypto.randomUUID() }])
-  const [currentBoardId, setCurrentBoardId] = useState(null)
-
-
+  const [boards, setBoards] = useState(() => {
+    const savedBoards = localStorage.getItem('boards')
+    return savedBoards ? JSON.parse(savedBoards) : [{ ...boardTemplate, boardId: crypto.randomUUID() }]
+  })
   useEffect(() => {
-    if (!currentBoardId && boards.length > 0) {
-      setCurrentBoardId(boards[0].boardId)
-    }
+    localStorage.setItem('boards', JSON.stringify(boards))
   }, [boards])
+
+
+  const [currentBoardId, setCurrentBoardId] = useState(null)
+  useEffect(() => {
+    setCurrentBoardId(boards[boards.length - 1].boardId)
+  }, [])
+
+
+  const [isCheckedStickersPanelShow, setIsCheckedStickersPanelShow] = useState(() => {
+    const savedIsCheckedPanel = localStorage.getItem('isCheckedPanel')
+    return savedIsCheckedPanel ? JSON.parse(savedIsCheckedPanel) : true
+  })
+  useEffect(() => {
+    localStorage.setItem('isCheckedPanel', JSON.stringify(isCheckedStickersPanelShow))
+  }, [isCheckedStickersPanelShow])
+
+
+  const [datedNextDaysValue, setDatedNextDaysValue] = useState(() => {
+    const savedNextDaysValue = localStorage.getItem('nextDaysValue')
+    return savedNextDaysValue ? JSON.parse(savedNextDaysValue) : 'all'
+  })
+  useEffect(() => {
+    localStorage.setItem('nextDaysValue', JSON.stringify(datedNextDaysValue))
+  }, [datedNextDaysValue])
 
 
   const [searchValue, setSearchValue] = useState('')
   const [checkedStickerId, setCheckedStickerId] = useState(null)
-  const [isCheckedStickersPanelShow, setIsCheckedStickersPanelShow] = useState(true)
   const [currentBoardPanel, setCurrentBoardPanel] = useState('board')
-  const [datedNextDaysValue, setDatedNextDaysValue] = useState('all')
   const [selectedFilterButton, setSelectedFilterButton] = useState(null)
   const [checkedOrderCounter, setCheckedOrderCounter] = useState(0)
   const [activeStickerId, setActiveStickerId] = useState(null)
--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,11 +1,19 @@
 
 
 import { ThemeContext } from '../contexts/ThemeContext.jsx'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function ThemeProvider({ children }) {
 
-  const [theme, setTheme] = useState('darkTheme')
+  const [theme, setTheme] = useState(()=>{
+    const savedTheme = localStorage.getItem('theme')
+    return savedTheme ?? 'darkTheme'
+  })
+
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme)
+  }, [theme])
 
 
   return (
