64cb82170e40691765b16904941d1f4c282f9fa9
Srdan Cakalj <srdjancakalj14@gmail.com>
2026-01-15 16:14:46 +0100

    feat: scroll to new list item when create new board

--- src/components/main/sidebar/new-board/board-list/board-list-item/BoardListItem.jsx
+++ src/components/main/sidebar/new-board/board-list/board-list-item/BoardListItem.jsx
@@ -1,7 +1,7 @@
 
 
 import styles from './boardListItem.module.css'
-import { useContext } from 'react'
+import { useContext, useRef, useEffect } from 'react'
 import { ClickOutsideElementContext } from '../../../../../../contexts/ClickOutsideElementContext.jsx'
 import { MainContext } from '../../../../../../contexts/MainContext.jsx'
 import Input from './board-list-item-elements/BoardListItemInput.jsx'
@@ -12,7 +12,17 @@ export default function BoardListItem({ mappedBoard }) {
 
   const { isClickedOutsideBoardList } = useContext(ClickOutsideElementContext)
   const { currentBoardId } = useContext(MainContext)
+  const itemRef = useRef(null)
 
+
+  useEffect(() => {
+    if (itemRef.current) {
+      itemRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest'
+      })
+    }
+  }, [currentBoardId])
 
 
   return (
@@ -21,6 +31,7 @@ export default function BoardListItem({ mappedBoard }) {
         ${styles.container} 
         ${mappedBoard.boardId === currentBoardId ? styles.selected : styles.noSelected}
         `}
+      ref={mappedBoard.boardId === currentBoardId ? itemRef : null}
     >
       {mappedBoard.isInput && !isClickedOutsideBoardList
         ? <Input mappedBoard={mappedBoard} />
